9855da4c3da218a2b61cd06c4c372f0b88b84d94
Hiren Joshi <hirenj@gmail.com>
2015-04-17 15:53:01 +0200

    Output improvement

--- js/main.js
+++ js/main.js
@@ -19,6 +19,16 @@ if (nconf.get('MS_DEBUG') || nconf.get('debug')) {
     gui.Window.get().showDevTools();
 }
 
+(function() {
+    var win = gui.Window.get();
+    var nativeMenuBar = new gui.Menu({ type: "menubar" });
+    try {
+        nativeMenuBar.createMacBuiltin("My App");
+        win.menu = nativeMenuBar;
+    } catch (ex) {
+        console.log(ex.message);
+    }
+})();
 var files_to_open = nconf.get('_') || [];
 if (files_to_open.length < 1) {
     document.getElementById('fileDialog').click();
@@ -687,17 +697,49 @@ var onlyUnique = function(value, index, self) {
 
 var pbcopy = function(data) { var proc = require('child_process').spawn('pbcopy'); proc.stdin.write(data); proc.stdin.end(); }
 
-var print_pep = function(protein,pep) { return [ protein, pep.sequence, pep.quant, pep.composition.join(',') ].join('\t'); };
+var print_pep = function(protein,pep) { return [ protein, pep.sequence, pep.quant, pep.composition.map(function(comp) { return comp.replace(/x.*/,''); }).join(',') ].join('\t'); };
 
-var filter_global_results = function(filter) {
-return Object.keys(global_datablock).map(function(prot) {
-    var peps = global_datablock[prot].filter(filter);
+var filter_global_results = function(datablock,filter) {
+if ( typeof datablock === 'function' ) {
+    filter = datablock;
+    datablock = global_datablock;
+}
+if ( ! filter ) {
+    filter = function() { return true; };
+}
+return "uniprot\tsequence\tquant\tglyco\n"+
+Object.keys(datablock).map(function(prot) {
+    var peps = datablock[prot].filter(filter);
     if (peps.length > 0) {
         return peps.map( partial(print_pep,prot) ).join("\n");
     }
     return "";
 }).filter(function(val) { return val != ''; }).join("\n");
 };
+
+var find_protein = function(peps,uniprot) {
+    return peps.filter(function(pep) { return pep.uniprot.indexOf(uniprot) >= 0; });
+};
+
+var find_sequence = function(peps,sequence) {
+    return peps.filter(function(pep) { return pep.Sequence.indexOf(sequence) == 0; });
+};
+
+var find_protein_sequence = function(peps,uniprot,sequence) {
+    return find_sequence(find_protein(peps,uniprot),sequence);
+};
+
+
+var singlets_filter = function(pep) {
+    return pep.quant === 0.00001 || pep.quant === 100000 || pep.quant === 'conflicting_singlets' || pep.quant === 'potential_medium' || pep.quant === 'potential_light';
+};
+
+var collect_galnac_ratios = function(peps) {
+    if ( ! peps ) {
+        peps = global_results;
+    }
+    return peps.filter(function(pep) { return pep.galnac_intensity > 0; }).map(function(pep) { return pep.galnac_intensity / pep.glcnac_intensity; }).join("\t");
+}
 
 
 var global_results;
@@ -743,6 +785,10 @@ var db = new sqlite3.Database(files_to_open[0],sqlite3.OPEN_READONLY,function(er
         }).then(function(peps) {
             console.log("Got all data");
             global_datablock = combine_all_peptides(peps);
+            if (nconf.get('write-peptides')) {
+                process.stdout.write(filter_global_results(function(pep) { return true; }));
+                gui.App.quit();
+            }
         }).catch(function(err) { console.log(arguments); console.log("Rejected"); });
 
     });
