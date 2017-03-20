2677260c42e5fbdf046b16226f103cf044451062
Hiren Joshi <hirenj@gmail.com>
2017-03-20 15:19:02 +0100

    Add in version updating

--- bin/run_conversion.sh
+++ bin/run_conversion.sh
@@ -6,7 +6,7 @@ cd "$1" 2>/dev/null || return $?  # cd to desired directory; if fail, quell any 
 echo "`pwd -P`" # output full, link-resolved path
 }
 
-optspec=":io-:"
+optspec=":ion-:"
 while getopts "$optspec" optchar; do
     case "${optchar}" in
         -)
@@ -19,6 +19,15 @@ while getopts "$optspec" optchar; do
                     val=${OPTARG#*=}
                     opt=${OPTARG%=$val}
                     input_dir="`dir_resolve \"$val\"`"
+                    ;;
+                nonetwork)
+                    val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+                    nonetwork="--offline"
+                    ;;
+                nonetwork=*)
+                    val=${OPTARG#*=}
+                    opt=${OPTARG%=$val}
+                    nonetwork="--offline"
                     ;;
                 output)
                     val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
@@ -39,6 +48,9 @@ while getopts "$optspec" optchar; do
             echo "usage: $0 [--input[=]<value>] [--output[=]<value>]" >&2
             exit 2
             ;;
+        n)
+            nonetwork="--offline"
+            ;;
         *)
             if [ "$OPTERR" != 1 ] || [ "${optspec:0:1}" = ":" ]; then
                 echo "Non-option argument: '-${OPTARG}'" >&2
@@ -52,6 +64,10 @@ if [[ -z "$input_dir" || -z "$output_dir" ]]; then
 	exit 1
 fi
 
+if [ ! -z `which grunt` ]; then
+    grunt version
+fi
+
 for recipe_dir in `find "${input_dir}" -type f -name '*.recipe.json' -exec dirname {} \; | grep -v '^\.' | uniq`; do
     runrecipe --input "$recipe_dir" --output "$output_dir" --nomangle
     if [ $? -gt 0 ]; then
@@ -59,6 +75,6 @@ for recipe_dir in `find "${input_dir}" -type f -name '*.recipe.json' -exec dirna
     fi
 done
 
-find "${input_dir}" -name "manifest*.xlsx" -exec node js/main.js --manifest {} --outputdir "$output_dir" --prefix-basename \;
+find "${input_dir}" -name "manifest*.xlsx" -exec node js/main.js --manifest {} --outputdir "$output_dir" "$nonetwork" --prefix-basename \;
 
 
